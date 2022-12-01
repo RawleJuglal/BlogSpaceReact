c4b70242853a1194ed3e91b3c27462267e39e1e1
Rawle Juglal <rawledjuglal@gmail.com>
2022-12-01 17:21:07 -0600

    skeleton
    BlogspaceReact project react skeleton

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="--app-app-container">
+      <h1>Is Working</h1>
+    </div>
+  )
+}
+
+export default App
